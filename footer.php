f3e967e9491c3c6f4ea7da335df36d3739da1b59
YumaMorimoto <kanten520212@gmail.com>
2023-02-19 18:49:49 +0900

    init wordpress

---
+++ footer.php
@@ -0,0 +1,11 @@
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.3/js/swiper.min.js"></script>
+    <script type="text/javascript" src="assets/js/main.js"></script>
+
+    <footer>
+
+    </footer>
+    <script type="text/javascript" src="assets/main.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="<?php echo get_theme_file_uri('src/favicon.ico'); ?>">
+    <!-- Swiper.js 4.3.3 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.3/css/swiper.min.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" media="all">
+    <title>SIDEVERG</title>
+    <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Economica&display=swap" rel="stylesheet">
+    <?php wp_head(); ?>
+</head>
+<body>
+    <header>
+
+    </header>
