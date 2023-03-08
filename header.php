d4e7067510449f4806f8302a2108451b603c8b63
YumaMorimoto <kanten520212@gmail.com>
2023-03-08 11:43:43 +0900

    Update headの更新

--- header.php
+++ header.php
@@ -4,16 +4,27 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Waseda Cinema Circle</title>
         <link rel="shortcut icon" href="<?php echo get_theme_file_uri('src/favicon.ico'); ?>">
+
+        <title><?php echo get_option('blogname', true); ?></title>
+        <meta name="description" content="<?php echo get_option('blogdescription', true) ?>">
+        
         <!-- font-family: 'Economica', Google Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Economica&display=swap" rel="stylesheet">
+        
+        <!-- font-family: 'Economica ital'-->
+        <link href="https://fonts.googleapis.com/css2?family=Economica:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+        
         <!-- font-family: 'Noto Serif JP', Google Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200;300;400;500;600;700;900&display=swap" rel="stylesheet">
+    
         <!-- Swiper.js 4.3.3 -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.3/css/swiper.min.css" />
+
         <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" media="all">
+        
         <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
+
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <?php wp_head(); ?>
@@ -25,14 +36,22 @@
         <section id="header">
             <header>
                 <div class="header_wrapper">
-                    <img class="header-logo" src="<?php echo get_theme_file_uri('src/logo.svg'); ?>" alt="" >
-                    <h1 class="header-title">Waseda Cinema Circle</h1>
+                    <img class="header-logo" src="<?php echo get_theme_file_uri('src/logo.svg'); ?>" alt="" onclick="location.href='./'">
+                    <h1 class="header-title">Waseda Cinemax Circle</h1>
                     <nav class="header-nav">
                         <ul class="header-menu">
-                            <li class="header-menu-item"><a href="#">Twitter</a></li>
-                            <li class="header-menu-item"><a href="#">Instagram</a></li>
-                            <li class="header-menu-item"><a href="#">Blog</a></li>
-                            <li class="header-menu-item"><a href="#" id="contact">Contact</a></li>
+                            <li class="header-menu-item">
+                                <a href="https://twitter.com/cinemaxsidevarg?lang=ja" id="twitter" target="_blank" rel="noopener noreferrer">Twitter</a>
+                            </li>
+                            <li class="header-menu-item">
+                                <a href="https://www.instagram.com/cinemaxsidevarg/" target="_blank" rel="noopener noreferrer">Instagram</a>
+                            </li>
+                            <li class="header-menu-item">
+                                <a href="#">Blog</a>
+                            </li>
+                            <li class="header-menu-item">
+                                <a href="#" id="contact">Contact</a>
+                            </li>
                         </ul>
                     </nav>
                 </div>
