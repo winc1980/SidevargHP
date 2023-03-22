de1d6fd38b4c75838ae9759526edc2e5c6f63bae
YumaMorimoto <kanten520212@gmail.com>
2023-03-23 00:49:39 +0900

    add pagination

--- page-list.php
+++ page-list.php
@@ -3,11 +3,11 @@
 get_header();
 ?>
 
-  <div class="eachblog_div">
-      <h1>Blog</h1>
-      <!-- <div class="blogichiran_txt1">Blog</div>
-      <div class="blogichiran_txt2">ブログ</div> -->
-  </div>
+    <div class="bloglist_div">
+        <h1>Blog</h1>
+        <!-- <div class="blogichiran_txt1">Blog</div>
+        <div class="blogichiran_txt2">ブログ</div> -->
+    </div>
 
   <div class="bloglist_cards container">
 
@@ -15,12 +15,12 @@ get_header();
     $args = array(
         'posts_per_page' => 10,
         'order'          => 'ASC',
+        'paged'          => max( 1, get_query_var('paged') ),
     );
-
-    $myposts = get_posts( $args );
-
-    foreach ( $myposts as $post ):
-        setup_postdata( $post );
+    $the_query = new WP_Query( $args );
+    if ( $the_query->have_posts() ):
+    while ( $the_query->have_posts() ):
+    $the_query->the_post();
     ?>
     <div class="blogichiran_box1" onclick="location.href='<?php the_permalink(); ?>'" >
         <div class="blogichiran_div1"><?php echo get_the_date( 'Y.m.d', get_the_ID() ); ?></div>
@@ -32,31 +32,32 @@ get_header();
                 <p><?php echo get_the_excerpt(); ?></p>
             </div>
     </div>
-    <?php endforeach; ?>
+    <?php
+    endwhile;
+    endif;
+    ?>
 
-  </div>
-
-  <?php
-  $args = array();
-
-  paginate_links( $args );
-  wp_reset_postdata();
-  ?>
+    </div>
 
     <!-- サンプル -->
     <!-- Font Awesome 5  -->
-    <div class="pageNation">
-      <div>
-        <i class="fa-solid fa-angles-left"></i>
-        <i class="fa-solid fa-angle-left"></i>
-      </div>
-      <div>
-        <p>123</p>
-      </div>
-      <div>
-        <i class="fa-solid fa-angle-right"></i>
-        <i class="fa-solid fa-angles-right"></i>
-      </div>
+    <div class="pagination">
+        
+        <?php
+        $pagination = paginate_links(
+            array(
+                'base'    => '%_%',
+                'format'  => home_url('?paged=%#%'),
+                'type'    => 'list',
+                'current' => max( 1, get_query_var('paged') ),
+                'total'   => $the_query->max_num_pages,
+                'prev_text' => __('<i class="fa-solid fa-angles-left"></i>'),
+                'next_text' => __('<i class="fa-solid fa-angle-right"></i>'),
+            )
+        );
+        echo $pagination;
+        ?>
+        
     </div>
 
     <!-- icons -->
